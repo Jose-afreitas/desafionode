d01f660ea4836588a994c343634834878647c566
Jose-afreitas <josealmeida106@gmail.com>
2024-03-08 13:18:57 -0300

    feat: :heavy_plus_sign: Adding dependencies, nodemon and morgan

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 const rotaProdutos = require('./src/routes/produtos')
 const rotaPedidos = require('./src/routes/pedidos')
 
-
+app.use(morgan("dev"));
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
+
+
+app.use((req, res, next) =>{
+    const erro = new Error("Não encontrado")
+    erro.send =404;
+    next(erro)
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500)
+
+    return res.send({
+        erro:{
+            message: error.message
+        }
+    })
+})
+
+
 
 module.exports = app;
